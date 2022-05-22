3d1d66863c58d5c989a651fa2c52be00a4754628
Elmer <ielmer.oliveros@gmail.com>
2022-05-22 18:21:39 -0500

    3

--- EO_scripts.R
+++ EO_scripts.R
@@ -6,6 +6,9 @@ install.packages("plotrix")
 install.packages("pie3D")
 install.packages("lubridate",dependencies = TRUE)
 install.packages('forecast')
+install.packages('gganimate')
+install.packages('gifski')
+#install.packages('gapminder')
 
 library(forecast)
 library(lubridate)
@@ -13,6 +16,9 @@ library(tidyverse)
 library(readr)
 library(markdown)
 library(plotrix)
+library(gganimate)
+library(gifski)
+#library(gapminder)
 
 ##Cargando el origen de datos
 Informe_de_virus <- read_delim("Informe de virus.csv", 
@@ -50,6 +56,11 @@ ft$tiempo=format(ft$fecha_tiempo,format = "%H:%M")
 
 ##qplot(data=ft, x=ft$fecha, y=ft$tiempo)
 qplot(data=ft, x=ft$fecha)
+#ggplot(data=ft,
+#       aes(x=ft$fecha,
+#           y=conteo)) +
+#  geom_line()
+
 ##qplot(data=ft, x=ft$tiempo)
 
 ##Quinta pregunta
@@ -119,4 +130,75 @@ ggplot(data=tseleccion_hora2,
        aes(x=horas2,
            y=conteo2)) +
   geom_line()
+
+###########################################
+
+##Sexta consulta
+
+tseleccion3=tiempo_detectado%>%
+  mutate(fecha_hora3=parse_date_time(tiempo_detectado$`Detected at`,"dmy HM"))%>%
+  group_by(horas3=floor_date(fecha_hora3,unit = 'hour'))%>%
+  summarise(conteo3=n())
+
+tseleccion4=tiempo_detectado%>%
+  mutate(fecha_hora3=parse_date_time(tiempo_detectado$`Detected at`,"dmy HM"))%>%
+  group_by(horas3=floor_date(fecha_hora3,unit = 'hour'))%>%
+  summarise(conteo3=n())
+
+tiempo_detectado
+glimpse(tiempo_detectado)
+
+fecha_momento=parse_date_time(tiempo_detectado$`Detected at`,"dmy HM")
+
+df=as.data.frame(fecha_momento)
+df$fecha=date(df$fecha_momento)
+df$tiempo=format(df$fecha_momento,format = "%H:%M")
+
+dt=as.character(df$tiempo)
+class(dt)
+#du=df%>%df$tiempo%>%group_by(df$tiempo)
+
+du=dt%>%select(dt$tiempo)%>%group_by(dt$tiempo)%>%summarise(nnumeros=n())
+
+qplot(data=df, x=df$fecha, y=df$tiempo)
+qplot(data=df, x=df$fecha)
+qplot(data=df, x=df$tiempo)
+
+ggplot(df, aes(x=`fecha`, y=`tiempo`)) + geom_line()
+
+
+
+
+#####comprobando la prediccion 
+
+tseleccion10=tiempo_detectado%>%mutate(fecha_hora10=parse_date_time(tiempo_detectado$`Detected at`,"dmy HM"))%>%
+  group_by(horas10=floor_date(fecha_hora10,unit = 'hour'))%>%
+  summarise(conteo10=n())
+
+## rellenando lo ceros
+horas_completas10=data.frame(
+  horas10=seq(floor_date(min(tseleccion10$horas10),unit = 'hour'),
+             floor_date(max(tseleccion10$horas10),unit = 'hour'),
+             by='hour'))
+
+##left join con horas
+tseleccion_hora10=horas_completas10%>%group_by(horas_redondeadas10=floor_date(horas10,unit = 'hour'))%>%
+  left_join(tseleccion10)%>%
+  mutate(conteo10= ifelse(is.na(conteo10),0,conteo10))
+##grafica inicial
+ggplot(data=tseleccion_hora10,
+       aes(x=horas10,
+           y=conteo10)) +
+  geom_line() + transition_reveal(horas10)
+
+
+
+
+
+
+
+
+
+
+
 
